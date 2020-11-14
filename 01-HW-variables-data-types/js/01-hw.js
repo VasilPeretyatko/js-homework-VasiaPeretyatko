e060a5ea10b1b6c4f50c3f55db8613398cd02c96
Vasya Emans <imagvas@gamil.com>
2020-11-14 20:59:32 +0200

    refactoring I, 1)changed variables of prices let to const, 2)fixed Math.random 3) some other fixes .toFixed, variable - sumTariffPlanWithoutCoins

--- 01-HW-variables-data-types/js/01-hw.js
+++ 01-HW-variables-data-types/js/01-hw.js
@@ -1,10 +1,9 @@
 //Base
 
-let tariffPlanYahoo = 15.678;
-let tariffPlanGoogle = 90.2345;
-let tariffPlanApple = 123.965;
-let moneyPay = 500;
-//let moneyPay = +prompt("Введіть суму оплати", 500);
+ const tariffPlanYahoo = 15.678;
+ const tariffPlanGoogle = 90.2345;
+ const tariffPlanApple = 123.965;
+ const moneyPay = 500;
 
 const maxTariffPlan = Math.max(tariffPlanYahoo, tariffPlanGoogle, tariffPlanApple);
 console.log(maxTariffPlan);
@@ -21,7 +20,7 @@ console.log(sumTariffPlanWithoutCoins);
 const sumTariffPlanRoundToHundredths = Math.round((Math.floor(tariffPlanYahoo) + Math.floor(tariffPlanGoogle) + Math.floor(tariffPlanApple))/100)*100;
 console.log(sumTariffPlanRoundToHundredths);
 
-const sumTariffPlanIsAnEven = ((sumTariffPlanWithoutCoins % 2) == 0 ) ? true : false;
+const sumTariffPlanIsAnEven = (sumTariffPlanWithoutCoins % 2) === 0 ? true : false;
 console.log(sumTariffPlanIsAnEven);
 
 const tariffPlanChangeMoney = moneyPay - sumTariffPlan;
@@ -30,12 +29,21 @@ console.log(tariffPlanChangeMoney);
 const tariffPlanAverage = Number((sumTariffPlan / 3).toFixed(2));
 console.log(tariffPlanAverage);
 
-const discountRandom = Number(Math.random().toFixed(2));
-const discountPercent = `${(discountRandom * 100).toFixed(0)} %`;
+//Вариант 1 (мой)
+// const discountRandom = Number(Math.random().toFixed(2));
+// const discountPercent = `${Math.trunc(discountRandom * 100)} %`;
+// console.log(discountRandom);
+// console.log(discountPercent);
+
+//Вариант 2 (супорта)
+const max = 99;
+const min = 1;
+const discountRandom = Math.floor((Math.random() * (max - min + 1)) + min) / 100;
+const discountPercent = `${discountRandom * 100} %`;
 console.log(discountRandom);
 console.log(discountPercent);
 
-const tariffPlanWithDiscount = Number((sumTariffPlan - (sumTariffPlan * discountRandom)).toFixed(2));
+const tariffPlanWithDiscount = Number((sumTariffPlan - (sumTariffPlan * (discountRandom))).toFixed(2));
 console.log (tariffPlanWithDiscount);
 
 const netProfit = Number(((sumTariffPlan / 2) - (sumTariffPlan * discountRandom)).toFixed(2));
@@ -47,7 +55,7 @@ const resultInfo = `
 Мінімальна ціна: ${minTariffPlan}
 Сума покупки: ${sumTariffPlan}
 Сума покупки без копійок: ${sumTariffPlanWithoutCoins}
-Округлення суми покупки до сотені: ${sumTariffPlanRoundToHundredths}
+Округлення суми покупки до сотень: ${sumTariffPlanRoundToHundredths}
 Сума всіх товарів - парне число: ${sumTariffPlanIsAnEven}
 Здача : ${tariffPlanChangeMoney}
 Середнє значення ціни товару: ${tariffPlanAverage}
@@ -56,5 +64,4 @@ const resultInfo = `
 Сума до оплати зі знижкою: ${tariffPlanWithDiscount}
 Чистий прибуток : ${netProfit}
 `;
-
 console.log(resultInfo);
